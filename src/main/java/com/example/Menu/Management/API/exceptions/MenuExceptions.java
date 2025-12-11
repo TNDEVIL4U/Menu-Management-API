9793d78de62a54886abd1d77c25f5767cb1cff72
T S BHUVANESHWAR <bhuvaneshwarts1@gmail.com>
2025-12-11 08:00:23 +0530

    Delete src directory

--- src/main/java/com/example/Menu/Management/API/exceptions/MenuExceptions.java
+++
@@ -1,4 +0,0 @@
-//package com.example.Menu.Management.API.exceptions;
-//
-//public class MenuExceptions {
-//}
--- src/main/java/com/example/Menu/Management/API/repository/MenuRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Menu.Management.API.repository;
-
-import com.example.Menu.Management.API.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<Item,Integer> {
-
-}
